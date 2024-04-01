7d3bbe4f78a50882be976c812f7d3b3d1fcf25c5
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2024-04-01 12:37:50 -0300

    start insert new book

--- Book.cpp
+++ Book.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+const std::string fileName = "./books.csv";
 
 struct Book {
     int id;
@@ -14,19 +15,42 @@ inline bool fileExists(const std::string& name) {
     else return false;
 }
 
+int getBookID() {
+    int id;
+    std::ifstream f(fileName);
+    std::string line;
+    if (f.is_open()) {
+        int count = 0;
+        while (std::getline(f, line)) {
+            if (count>0) { // skip csv header
+                
+            }
+            count++;
+        }
+    } else id = 1;
+    f.close();
+    return id;
+}
+
+Book getBookData() {
+    Book newBook;
+    std::cout<<"Name: "; std::cin>>newBook.name; std::cout<<"\n";
+    std::cout<<"Author: "; std::cin>>newBook.author; std::cout<<"\n";
+    std::cout<<"Price: "; std::cin>>newBook.price; std::cout<<"\n";
+    return newBook;
+}
+
 void saveBook(const Book& b) {
-    const std::string fileName = "./books.csv";
     std::ofstream booksFile;
     if (fileExists(fileName)) {
         booksFile.open(fileName, std::ios::app);
         booksFile<<b.id<<","<<b.name<<","<<b.author<<","<<b.price<<"\n";
-        booksFile.close();
     } else {
         booksFile.open(fileName);
         booksFile<<"id,name,author,price"<<"\n";
         booksFile<<b.id<<","<<b.name<<","<<b.author<<","<<b.price<<"\n";
-        booksFile.close();
     }
+    booksFile.close();
 }
 
 std::ostream& operator<<(std::ostream& out, const Book& b) {
@@ -44,7 +68,8 @@ int main() {
     estudo_naa.author = "Unknown";
     estudo_naa.price = 289.9;
 
-    std::cout<<estudo_naa;
+    Book newBook = getBookData();
 
     saveBook(estudo_naa);
+    saveBook(newBook);
 }
