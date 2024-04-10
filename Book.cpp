c1100838b76b20569f9a89bfa49567439e1378f7
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2024-04-10 09:01:39 -0300

    create book class

--- Book.cpp
+++ Book.cpp
@@ -4,70 +4,83 @@
 #define DELIMITER ','
 using namespace std;
 
-struct Book {
-    unsigned int id;
-    string name;
-    string author;
-};
-
-inline bool fileExists(const string& name) {
-    ifstream f(name);
+/*
+Receive a path to a file and return true if exists and false if doesn't.
+*/
+inline bool fileExists(const string& path) { // inline function is expanded in line when is called, may incriese performance if is a small function
+    ifstream f(path);
     if (f.is_open()) return true;
     else return false;
 }
-/*
-Search if book csv file exists, then return the proper id for register a new book
-*/
-int getBookID() {
-    string line;
-    unsigned int id;
-    char ch;
-    ifstream f(BOOKS_FILENAME);
-    if (f.is_open()) {
-        f.seekg(-1, ios::end); // go to the last position from the end of file (before EOF)
-        while (true) {
-            f.get(ch);
-            if (f.tellg() <= 1) { // if the position is at the beginning of file
-                f.seekg(0); // there is only one line, the go to the beginning
-                break;
-            } else if (ch == '\n') { // reach the beginnig of last line
-                break;
+
+class Book {
+    public:
+        unsigned int id;
+        string name;
+        string author;
+
+        /*
+        Object initializer will ask the name of the book and the author when object is crated.
+        */
+        Book() {
+            id = getBookID();
+            cout<<"Name: "; cin>>name; cout<<"\n";
+            cout<<"Author: "; cin>>author; cout<<"\n";
+        }
+
+        /*
+        If Book file already exists will append the file creating new lines on csv file,
+        if Book file doesn't exists will crate a new file.
+        */
+        void saveBook() {
+            ofstream booksFile;
+            if (fileExists(BOOKS_FILENAME)) {
+                booksFile.open(BOOKS_FILENAME, ios::app);
+                booksFile<<"\n"<<id<<DELIMITER<<name<<DELIMITER<<author;
             } else {
-                f.seekg(-2, ios::cur); // go back one character from the current position
+                booksFile.open(BOOKS_FILENAME);
+                booksFile<<"id"<<DELIMITER<<"name"<<DELIMITER<<"author";
+                booksFile<<"\n"<<id<<DELIMITER<<name<<DELIMITER<<author;
             }
+            booksFile.close();
         }
-        getline(f, line); // get last line
-        for (int i=1; i<line.length(); i++) {
-            if (line[i] == DELIMITER) { // search for first delimiter
-                id = stoi(line.substr(0,i))+1; // get substring of id, convert to integer and add 1
-                break;
-            }
+
+    private:
+        /*
+        Search if book csv file exists, then return the proper id for register a new book
+        */
+        int getBookID() {
+            string line;
+            unsigned int idCollected;
+            char ch;
+            ifstream f(BOOKS_FILENAME);
+            if (f.is_open()) {
+                f.seekg(-1, ios::end); // go to the last position from the end of file (before EOF)
+                while (true) {
+                    f.get(ch);
+                    if (f.tellg() <= 1) { // if the position is at the beginning of file
+                        f.seekg(0); // there is only one line, the go to the beginning
+                        break;
+                    } else if (ch == '\n') { // reach the beginnig of last line
+                        break;
+                    } else {
+                        f.seekg(-2, ios::cur); // go back one character from the current position
+                    }
+                }
+                getline(f, line); // get last line
+                for (int i=1; i<line.length(); i++) {
+                    if (line[i] == DELIMITER) { // search for first delimiter
+                        idCollected = stoi(line.substr(0,i))+1; // get substring of id, convert to integer and add 1
+                        break;
+                    }
+                }
+                f.close();
+            } else idCollected = 1;
+            return idCollected;
         }
-        f.close();
-    } else id = 1;
-    return id;
-}
 
-Book getBookData() {
-    Book newBook;
-    newBook.id = getBookID();
-    cout<<"Name: "; cin>>newBook.name; cout<<"\n";
-    cout<<"Author: "; cin>>newBook.author; cout<<"\n";
-    return newBook;
-}
 
-void saveBook(const Book& b) {
-    ofstream booksFile;
-    if (fileExists(BOOKS_FILENAME)) {
-        booksFile.open(BOOKS_FILENAME, ios::app);
-        booksFile<<"\n"<<b.id<<DELIMITER<<b.name<<DELIMITER<<b.author;
-    } else {
-        booksFile.open(BOOKS_FILENAME);
-        booksFile<<"id"<<DELIMITER<<"name"<<DELIMITER<<"author";
-        booksFile<<"\n"<<b.id<<DELIMITER<<b.name<<DELIMITER<<b.author;
-    }
-    booksFile.close();
-}
+};
 
 ostream& operator<<(ostream& out, const Book& b) {
     out<<"ID: "<<b.id<<"\n";
@@ -77,9 +90,11 @@ ostream& operator<<(ostream& out, const Book& b) {
 }
 
 int main() {
-    Book newBook = getBookData();
-    saveBook(newBook);
+    Book newBook;
+    newBook.saveBook();
+    cout<<newBook<<"\n\n";
 
-    Book newBook2 = getBookData();
-    saveBook(newBook2);
+    Book newBook2;
+    newBook2.saveBook();
+    cout<<newBook2<<"\n\n";
 }
