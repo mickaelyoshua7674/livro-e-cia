95ed3142b1189c3d7611eafcfc080be822974619
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2024-04-16 11:27:24 -0300

    password func

--- menus.cpp
+++ menus.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#define PASSWORD "4242"
 using namespace std;
 
-void BookMenu() {
+void bookMenu() {
 
 }
 
-int main() {
+void mainMenu() {
     int option;
 	cout<<"*************************************************"<<endl;
-	cout<<"         BOOKSHOP MANGEMENT SYSTEM"<<endl;
+	cout<<"      Sistema de Gerenciamento de Compras"<<endl;
 	cout<<"*************************************************"<<endl;
 	cout<<"   1. Livros"<<endl;
 	cout<<"   2. Compras"<<endl;
 	cout<<"   3. Sair"<<endl<<endl<<endl;
-	cout<<"Enter Your Choice : ";
+	cout<<"Escolha a opção: ";
 	cin>>option;
 	switch (option) {
         case 1:
@@ -21,9 +22,36 @@ int main() {
             BookMenu();
             break;
         case 3:
-            exit();
+            exit(EXIT_SUCCESS); // successful termination
         default:
             cout<<"Opção incorreta/inválida"<<endl;
             break;
     }
+}
+
+void password() {
+    int num = 0;
+    cout<<"Insira senha: ";
+
+    for (int i=0; i<4; i++) {
+        num = num*10+(getch()-48);
+        cout<<"*";
+    }
+    
+    if (num == PASSWORD) {
+        cout<<endl<<endl<<"Senha correta"<<endl<<endl;
+        cout<<"Pressione qualquer tecla...";
+        getch();
+    } else {
+        cout<<endl<<endl<<"Senha incorreta"<<endl<<endl;
+        cout<<"Pressione qualquer tecla...";
+        getch();
+        exit(EXIT_FAILURE);
+    }
+
+    return;
+}
+
+int main() {
+
 }
